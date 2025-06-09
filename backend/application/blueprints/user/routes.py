9f82165190b3791cbec2afd5ed1273f0e8527155
Sam Morgan <samvmorgan5@gmail.com>
2025-06-08 20:30:13 -0400

    Added full CRUD with tested routes

--- backend/application/blueprints/user/routes.py
+++ backend/application/blueprints/user/routes.py
@@ -54,3 +54,65 @@ def add_user():
    db.session.add(new_user)
    db.session.commit()
    return user_schema.jsonify(new_user), 201
+
+#token required to get all users
+@users_bp.route('/', methods=['GET'])
+@token_required
+def get_users(current_user_id):  # Accept the argument from the decorator
+    try:
+        page = int(request.args.get('page'))
+        per_page = int(request.args.get('per_page'))
+        query = select(User)
+        users = db.paginate(query, page=page, per_page=per_page)
+        return users_schema.jsonify(users), 200
+    except:
+        query = select(User)
+        result = db.session.execute(query).scalars().all()
+        return users_schema.jsonify(result), 200
+   
+
+#Token Required to update user
+@users_bp.route('/', methods=['PUT'])
+@token_required
+def update_user(current_user_id):
+    query = select(User).where(User.id == current_user_id)
+    user = db.session.execute(query).scalars().first()
+
+    if user is None:
+        return jsonify({"error": "User not found"}), 200
+    try:
+        user_data = user_schema.load(request.json, partial=True)  # allow partial updates
+    except ValidationError as e:
+        return jsonify(e.messages), 400
+
+    # Only check for email uniqueness if email is being changed, otherwise change what is being modified
+    if 'email' in user_data and user_data['email'] != user.email:
+        query = select(User).where(User.email == user_data['email'])
+        db_user = db.session.execute(query).scalars().first()
+        if db_user:
+            return jsonify({"error": "Email already exists"}), 400
+
+    for field, value in user_data.items():
+        if value is not None:
+            setattr(user, field, value)
+
+    db.session.commit()
+    return user_schema.jsonify(user), 200
+
+
+#Token Required to delete user
+@users_bp.route("/", methods=['DELETE'])
+@token_required
+def delete_user(user_id):
+   # Fetch the user using the user_id
+   query = select(User).where(User.id == user_id)
+   user = db.session.execute(query).scalars().first()
+
+   if not user:
+      return jsonify({"message": "User not found"}), 400
+
+   # Delete the User
+   db.session.delete(user)
+   db.session.commit()
+
+   return jsonify({"message":"User deleted successfully."}), 200
