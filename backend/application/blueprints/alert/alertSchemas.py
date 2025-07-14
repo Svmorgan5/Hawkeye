32a4f5c336bbf856a49b9b9f1d51f29a807e76b8
Sam Morgan <samvmorgan5@gmail.com>
2025-07-14 18:58:34 -0400

    updated alert schema

--- backend/application/blueprints/alert/alertSchemas.py
+++ backend/application/blueprints/alert/alertSchemas.py
@@ -8,7 +8,8 @@ from backend.application.extensions import ma
 class AlertSchema(ma.SQLAlchemyAutoSchema):
     alert_type = EnumField(AlertType, by_value=True, required=True)
     scheduled_time = fields.DateTime(allow_none=True)
-    # Mark institution as dump_only so it isn’t expected on input
+    location = fields.Str(required=True, allow_none=False)  # Add this line
+    # Mark institution as dump_only so it isn't expected on input
     institution = fields.Nested("InstitutionSchema", dump_only=True)
     
     class Meta:
@@ -16,7 +17,7 @@ class AlertSchema(ma.SQLAlchemyAutoSchema):
         include_relationships = True
         load_instance = True
         unknown = EXCLUDE   # ignore extra fields like camera_ids
-        dump_only = ("code", "institution_id", "location", "timestamp", "institution")
+        dump_only = ("code", "institution_id", "timestamp", "institution")  # Remove "location" from here
 
 alert_schema = AlertSchema()
 alerts_schema = AlertSchema(many=True)
