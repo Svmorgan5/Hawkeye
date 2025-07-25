299b6cede3e26f7e57c4b9e9ad5e3c2731331e4b
Sam Morgan <samvmorgan5@gmail.com>
2025-07-25 15:33:36 -0400

    pls dont loose power

--- src/Header/Header.tsx
+++ src/Header/Header.tsx
@@ -6,7 +6,7 @@ import { useNavigate } from 'react-router-dom';
 import React, { useEffect } from 'react';
 import {ToastContainer, toast} from 'react-toastify'
 import {io } from 'socket.io-client';
-improt useTokenContext
+
 
 const Header = () => {
 
